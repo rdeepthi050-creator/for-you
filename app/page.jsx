aaff2b1286a809f2bf9a2e1a4856538adf9c6ca9
rdeepthi050-creator <rdeepthi050@gmail.com>
2026-03-23 22:42:41 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,20 @@
 "use client"
+import { useState } from "react"
+
+import FirstScreen from "../FirstScreen"
 import SecondScreen from "../SecondScreen"
+import LyricsScreen from "../LyricsScreen"
+import OutroScreen from "../OutroScreen"
 
 export default function Home() {
-  return <SecondScreen />
+  const [step, setStep] = useState(1)
+
+  return (
+    <>
+      {step === 1 && <FirstScreen onNext={() => setStep(2)} />}
+      {step === 2 && <SecondScreen onNext={() => setStep(3)} />}
+      {step === 3 && <LyricsScreen onNext={() => setStep(4)} />}
+      {step === 4 && <OutroScreen />}
+    </>
+  )
 }
